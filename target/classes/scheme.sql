525e59f5b0a01c4ff5d505a992f65a26781340db
sebi_medina10 <sebimedina5@gmail.com>
2025-11-22 20:51:14 -0300

    Se crea el schema en SQL para la cración de la tabla Docentes en la base de datos

--- target/classes/scheme.sql
+++ target/classes/scheme.sql
@@ -6,4 +6,16 @@ CREATE TABLE users (
     id INTEGER PRIMARY KEY AUTOINCREMENT, -- Clave primaria autoincremental para SQLite
     name TEXT NOT NULL UNIQUE,          -- Nombre de usuario (TEXT es el tipo de cadena recomendado para SQLite), con restricción UNIQUE
     password TEXT NOT NULL           -- Contraseña hasheada (TEXT es el tipo de cadena recomendado para SQLite)
+);      
+
+-- Elimina la tabla 'docentes' si ya existe para asegurar un inicio limpio
+DROP TABLE IF EXISTS docentes;
+-- Crea la tabla 'docentes' con los campos originales, adaptados para SQLite.
+CREATE TABLE docentes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    dni INTEGER NOT NULL UNIQUE, 
+    apellido TEXT NOT NULL, -- Nombre de la persona (TEXT es el tipo de cadena recomendado para SQLite), con restricción UNIQUE
+    nombre TEXT NOT NULL,  -- Apellido de la persona.
+    email TEXT NOT NULL UNIQUE, --direccion de correo eléctronico
+    CHECK(dni>0)  
 );
