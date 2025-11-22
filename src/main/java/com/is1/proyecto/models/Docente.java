1ad293a04a056844b4934d88ac0ac3bf9c796b7e
sebi_medina10 <sebimedina5@gmail.com>
2025-11-22 20:42:31 -0300

    Se agrega la clase Docente que tendrá una tabla en la base de datos

---
+++ src/main/java/com/is1/proyecto/models/Docente.java
@@ -0,0 +1,9 @@
+package com.is1.proyecto.models;
+
+import org.javalite.activejdbc.Model;
+import org.javalite.activejdbc.annotations.Table;
+
+@Table("docentes") //name of table 
+public class Docente extends Model {
+    //Getters y setters de tables son creados automáticamente .
+}
